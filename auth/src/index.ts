fe9b4da16b36ee053171b6becc840160f4f82a6d
avner <avner@zebra-med.com>
2021-09-24 22:31:06 +0300

    validation ans structure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,7 @@
-import express from "express";
-import { json } from "body-parser";
 
-const app = express();
-app.use(json());
+// import AuthController from "./controllers/auth.controller";
+// import { App } from "./server";
 
-app.get('/currentuser', (req, res) => {
-return res.send('current user endpoints')
-});
-app.get('/', (req, res) => {
-    return res.send('dcdc')
-    });
-app.listen(3000, () => console.log('listening on port 3000!!')
-);
+// const app = new App([new AuthController()], 3000);
+
+// app.listen();
